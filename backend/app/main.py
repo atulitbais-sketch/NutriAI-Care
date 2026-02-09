088606abc24f310e03817fcb11885c049182bcd9
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-02-09 12:12:37 +0530

    Add backend code and README

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.api.lab import router as labs_router
+from app.api.health import router as health_router
+
 
 app = FastAPI()
 
 app.include_router(labs_router, prefix="/api")
+app.include_router(health_router, prefix="/api")
