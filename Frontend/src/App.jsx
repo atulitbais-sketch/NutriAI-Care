84d3bc4df8a9424f432f24e475ca1ad9b5fbe427
atulitbais-sketch <atulitbais@gmail.com>
2026-02-16 15:51:04 +0530

    Add styled login page and layout fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import Upload from "./pages/Upload";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/upload" element={<Upload />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,4 @@
+function Dashboard() {
+  return <h1>Welcome to Medi-Flow Dashboard</h1>;
+}
+export default Dashboard;
