1ae8f8b1bc80aad44df0df2c0fb014c99c6331b2
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-06-03 13:59:30 +0530

    auth changed

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,18 +1,12 @@
-import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
-axios.get(`${API_URL}/api/doctors`);
 export const loginUser = async (email, password) => {
-
   const res = await fetch(`${API_URL}/api/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      email,
-      password
-    })
+    body: JSON.stringify({ email, password })
   });
 
   if (!res.ok) {
